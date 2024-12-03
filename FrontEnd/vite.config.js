077a1ac0b0b96ccdae6605c363e106ff82cb66dc
shamsheedali <shamsheedhopz0786@gmail.com>
2024-12-04 00:04:32 +0530

    vite config changed

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use relative paths for assets
+  build: {
+    outDir: 'dist', // Output directory (default is "dist")
+    assetsDir: 'assets', // Directory for static assets within dist
+  },
 })

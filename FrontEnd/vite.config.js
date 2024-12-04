8c5384342f12378b627bc6410d86f7f7e3a78e1f
shamsheedali <shamsheedhopz0786@gmail.com>
2024-12-04 09:08:21 +0530

    Final commit before building

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -6,15 +6,16 @@
 //   plugins: [react()],
 // })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for assets
+  base: './', // Use relative paths for assets (good for production)
   build: {
     outDir: 'dist', // Output directory (default is "dist")
     assetsDir: 'assets', // Directory for static assets within dist
   },
-})
+});
+

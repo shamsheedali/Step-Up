365434d7d155853e33688d94bb3111499ce3fa81
shamsheedali <shamsheedhopz0786@gmail.com>
2024-12-05 21:32:42 +0530

    removed base from vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -12,7 +12,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for assets (good for production)
   build: {
     outDir: 'dist', // Output directory (default is "dist")
     assetsDir: 'assets', // Directory for static assets within dist

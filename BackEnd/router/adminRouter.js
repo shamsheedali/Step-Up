132126be09b2ec04f1a226fcf9740c3b6e894dbf
shamsheedali <shamsheedhopz0786@gmail.com>
2025-06-14 13:07:24 +0530

    feat: added search in user from backend with debounce

--- BackEnd/router/adminRouter.js
+++ BackEnd/router/adminRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, fetchUsers, blockUser, unBlockUser } from '../controller/adminController.js';
+import { login, fetchUsers, blockUser, unBlockUser, searchUsers } from '../controller/adminController.js';
 import verifyToken from '../middleware/middleware.js';
 import requireRole from '../middleware/requireRole.js';
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/admin-login', login);
 router.get('/all-users', verifyToken, requireRole("admin"), fetchUsers);
 router.patch('/:id/block', verifyToken, requireRole("admin"), blockUser);
 router.patch('/:id/unblock', verifyToken, requireRole("admin"), unBlockUser);
+router.get("/search-users", verifyToken, requireRole("admin"), searchUsers);
 
 export default router;
